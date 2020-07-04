fc22aee63af8e1e10f3b60c46a49963c28f2d5a6
Duong-spm <56970251+Duong-spm@users.noreply.github.com>
2020-07-04 10:56:35 +0700

    update new
    program

--- ManageShop/GUI/Program.cs
+++ ManageShop/GUI/Program.cs
@@ -19,7 +19,7 @@ namespace GUI
             Application.SetCompatibleTextRenderingDefault(false);
 
             BonusSkins.Register();
-            Application.Run(new NhanVienGUI());
+            Application.Run(new RegisterGUI());
         }
     }
 }
